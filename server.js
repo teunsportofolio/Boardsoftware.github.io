2a225f06cda2116f01309d488d0ad82d198ab42c
teunsportofolio <153618242+teunsportofolio@users.noreply.github.com>
2026-04-05 01:08:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -651,4 +651,10 @@ function computeSmoothness(positions) {
 
 
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Also, export the app for Vercel's builder
+module.exports = app;
